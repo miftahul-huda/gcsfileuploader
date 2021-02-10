bba8ce48d0b8eb6f5f9e2b96e4c0420ce0d7cfff
Miftahul Huda <miftahul.huda@devoteam.com>
2021-02-10 17:45:14 +0700

    init

--- initialization.js
+++ initialization.js
@@ -3,24 +3,21 @@ const UploadedFileModel  = require( './modules/models/uploadedfilemodel')
 
 
 const { Sequelize, Model, DataTypes } = require('sequelize');
-/*const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database/uploader.sqlite'
-});*/
 
 
-/*const sequelize = new Sequelize('gcsfileuploader', 'nodeuser', 'rotikeju98', {
-    host: '/cloudsql/mind-id-mct-dev:asia-southeast2:authentication-uploader',
-    dialect: 'postgres',
-    dialectOptions: {
-        socketPath: '/cloudsql/mind-id-mct-dev:asia-southeast2:authentication-uploader'
-    }
-  });*/
 
-  const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
+/*const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
     host: process.env.DBHOST,
     dialect: "postgresql"  
-});
+});*/
+
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
+    dialect: 'postgresql',
+    host: '/cloudsql/' + process.env.CLOUD_SQL_CONNECTION_NAME,
+    dialectOptions: {
+        socketPath: '/cloudsql/' + process.env.CLOUD_SQL_CONNECTION_NAME
+  },
+  });
 
 class Initialization {
     static async initializeDatabase(){
